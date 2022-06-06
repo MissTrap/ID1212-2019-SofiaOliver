cdcb6bfb7037e5ce94f4823a96e5b8b3a6f499a1
MissTrap <60643253+MissTrap@users.noreply.github.com>
2022-06-06 19:59:52 +0200

    Create Main.java

---
+++ src/client/main/Main.java
@@ -0,0 +1,8 @@
+//*Created by Sofia Söderström and Oliver Wilén 2019-10-29*//
+
+package client.main;
+import client.view.View;
+
+public class Main{
+    public static void main(String[] args){new View();}
+}
